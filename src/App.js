3896548c885b2f922083c117695282b73f0f28ce
viriatonova <viriatonova@megaterio.com.br>
2021-10-01 21:45:02 -0300

    react installation - hellou up

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Main from './components/Main';
+import './index.js';
+
+function App() {
+  return <Main />;
+}
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    render () {
+        return <h1>Hellou Word!</h1>;
+    }
+}
+export default Main;
