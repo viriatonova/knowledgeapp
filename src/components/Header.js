7381b3e98d2f4776a8093557d27763d48d88cd45
ViriatoNova <viriatosampaio@gmail.com>
2021-10-02 16:11:36 -0300

    Edit Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Box from '@mui/material/Box';
+import { flexbox } from "@mui/system";
 
 
-export default function Page404() {
+export default function Header() {
    
     return (
         <Box component="span" sx={{
             width: 'full',
             height: 'auto', 
-            padding: '3rem', 
+            padding: '2rem',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor:'black',
+            color:'White',
+            fontSize: '1.75rem'
         }}>
-            <h1>Questionarios Conhecimentos Gerais</h1>
+            Knowledge App
         </Box>
     );
     
