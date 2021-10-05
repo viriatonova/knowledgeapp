169d3d9932d1a8866e21b60aebe603566d9ea2cb
ViriatoNova <viriatosampaio@gmail.com>
2021-10-05 11:28:42 -0300

    Insert api call to App function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Routes from './routes';
 import Header from "./components/Header";
 import './index.js';
+import axios from 'axios';
 
+const baseURL = "https://opentdb.com/api.php?amount="
 
 
 export default function App() {
+  
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+      axios.get(`${baseURL}4`).then((response) => {
+          setQuestions(response.data);
+      });
+  }, []);
+
+  const apiResults = questions.results
+  console.log(apiResults);
+
+ 
+  localStorage.setItem("app", apiResults);
+  
+  
   return (
     <>
     
--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Container from '@mui/material/Container';
-import Adapter from "../Adapter";
-
 
 export default function Questions() {
 
    
     return (
         <Container maxWidth="xl">
-            <Adapter />
+
         </Container>
     );
     
