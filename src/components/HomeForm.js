e83c600216b1a74d1e20becc6567b7cedb7b2a2f
ViriatoNova <viriatosampaio@gmail.com>
2021-10-07 09:46:18 -0300

    fixed HomeForm

--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -1,36 +1,58 @@
-import React, { useContext } from "react";
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
+import React from "react";
 import FormLabel from '@mui/material/FormLabel';
-import { QuantContext } from "../context/QuantContext";
-
-export default function HomeForm() {
-    const { quant, setQuant } = useContext( QuantContext );
-    
-
-    const handleChange = (event) => {
-      setQuant(event.target.value);
-    };
-
-    console.log(quant)
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+// import { QuantContext } from "../context/QuantContext";
+import { Controller, useForm } from "react-hook-form";
+import { Select } from "@mui/material";
 
 
+const HomeForm = () => {
+  
+    // const { quant, setQuant } = useContext( QuantContext );
+    const { control, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data)
+      
     return (
-      <form> 
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Questions</FormLabel>
-          <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
-            <FormControlLabel value={2} control={<Radio />} label="2" />
-            <FormControlLabel value={5} control={<Radio />} label="5" />
-            <FormControlLabel value={10} control={<Radio />} label="10" />
-            <FormControlLabel value={15} control={<Radio />} label="15" />
-          </RadioGroup>
+      <form onSubmit={handleSubmit(onSubmit)}> 
+        <FormControl fullWidth>
+            <FormLabel component="legend">First Name</FormLabel>
+            <Controller
+              render={({ field }) => <TextField {...field} />}
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+              name="firstName"
+              control={control}
+              defaultValue=""
+            />
+            <FormLabel component="legend">Questions</FormLabel>
+            <Controller
+              render = {({ field }) =>
+                <Select {...field} >
+                  <MenuItem value={2}>
+                    2
+                  </MenuItem>
+                  <MenuItem value={5}>
+                    5
+                  </MenuItem>
+                  <MenuItem value={10}>
+                    10
+                  </MenuItem>
+                  <MenuItem value={15}>
+                    15
+                  </MenuItem>
+                </Select>
+              }
+              name="wordlevel"
+              control={control}
+              defaultValue=""
+            />
         </FormControl>
-        <button type="submit">Start</button>
+        <input type="submit"></input>
       </form>
       
   );
-  
 }
+export default HomeForm;
